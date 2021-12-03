28c9ea50cba4df6a9a0e948db67f591cabe9fc6d
daniriolo <72992100+daniriolo@users.noreply.github.com>
2021-12-03 12:02:12 -0600

    Update Practica1.c

--- Practica1/Practica1.c
+++ Practica1/Practica1.c
@@ -4,22 +4,22 @@
 /*  Program that receive as input two coordinates (x1,y1) y (x2,y2)
 	and gives as an output an image with the line
  */
-int abs(int n){
+int abs(int n){ //Absolute Value Function
 	return (n>0)? n:(n*(-1));
 }
 
-int max(int a,int b){
+int max(int a,int b){ //Maximum Value Function
 	return (a>b)? a : b;
 }
-int min(int a,int b){
+int min(int a,int b){ //Minimum Value Function
 	return (a<b)? a : b;
 }
 
-void SetPixel(int x,int y){
+void SetPixel(int x,int y){ //Function that prints the coordinates 
 	printf("[%d][%d] \n",x,y);
 }
  
-int main (){
+int main (){ //Main Function
 	int x1,x2,y1,y2;
 	printf("X1? :\n");
   	scanf("%d",&x1);
@@ -31,7 +31,7 @@ int main (){
   	scanf("%d",&y2);
   	printf("p1(%d,%d)\n",x1,y1);
   	printf("p2(%d,%d)\n",x2,y2);
-  	
+	//The calculation of the deltas
   	int dx= x2-x1;
   	int dy=y2-y1;
   	
@@ -53,8 +53,6 @@ int main (){
 		float m = dy/dx; //Slope
 		// Y incercept
 		float b= y1- m *x1;
-		
-		
 		if(abs(m)>1){//Steep
 			dy=abs(dy);
 			int y=max(y1,y2);
