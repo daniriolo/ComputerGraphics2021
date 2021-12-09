c21a596bbe46adf20057d45ec4ba8299f9d353f1
daniriolo <72992100+daniriolo@users.noreply.github.com>
2021-12-08 18:36:45 -0600

    Update Practica3.c

--- Practica 3/Practica3.c
+++ Practica 3/Practica3.c
@@ -1,1 +1,17 @@
+/*
+Serna Gonzalez Daniel
+3CV14
 
+From an .obj file , apply the final transformation matrix (with the
+basic operations; rotation,translation,scaling) to make frames in 
+.ppm and create an animation.
+
+*/
+
+#include <stdio.h>
+
+
+int main(){
+
+  return 0;
+}
